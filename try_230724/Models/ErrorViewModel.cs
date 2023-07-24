7aca552f9e990bebc56b95dc70c7db810b2bf618
Ethan-Tsai <ethan-tsai-0129@outlook.com>
2023-07-24 15:05:39 +0800

    c#_web

---
+++ try_230724/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace try_230724.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
